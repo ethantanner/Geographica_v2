c4093b48b7730ed99b3aaf5d666a1b475fd05eca
daviddevore11 <daviddevore11@augustana.edu>
2015-05-11 23:04:15 -0500

    Have finished the method that will display the current top 10 scores from the leader board which is stored in parse.  If your name cracks the top ten then you will have a "*" next to your name to show that you have made the top ten.  If you are below the top ten there will be a ". . ." line above your player's name to show this as well as your current position within the leader board.  Example it would be top ten, then a line ". . .", then 23. Dave 123453.  If there is time we can maybe make a formal leader boards layout to access from main menu.

--- app/src/main/java/edu/augustana/csc490/geographica_v2/EndGame.java
+++ app/src/main/java/edu/augustana/csc490/geographica_v2/EndGame.java
@@ -2,6 +2,7 @@ package edu.augustana.csc490.geographica_v2;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.text.InputType;
@@ -68,7 +69,7 @@ public class EndGame extends Activity {
             roundManager.styleTextView((TextView) findViewById(R.id.roundScoreView),"Total Score: " + scorePlayer1,24);
             //call to method that handles the parse calls.
             updatePlayerCount();
-            updateLeaderboard();
+            updateLeaderboard(this);
         }else{
             roundManager.styleTextView((TextView) findViewById(R.id.roundScoreView),"Player1 Score: " + scorePlayer1,24);
         }
@@ -85,23 +86,20 @@ public class EndGame extends Activity {
         query.getInBackground("FA6PbcJq7N", new GetCallback<ParseObject>() {
             @Override
             public void done(ParseObject playerCount, ParseException e) {
-                if(e == null){
+                if (e == null) {
                     int tempNum = (int) playerCount.getNumber("totalPlayers");
-                    Log.d("INFO", "Current Player Count: " + tempNum);
                     tempNum = tempNum + 1;
                     playerTotal = tempNum;
                     playerCount.put("totalPlayers", tempNum);
                     playerCount.saveInBackground();
-                    Log.d("INFO", "Current Player Count: " + tempNum);
-                    Log.d("INFO", "Player Name: " + playerName);
-                }else{
+                } else {
                     Log.d("QUERY", "Error in query for PlayerCount: " + e.getMessage());
                 }
             }
         });
     }
 
-    public void updateLeaderboard(){
+    public void updateLeaderboard(final Context context){
         final EditText input = new EditText(this);
         input.setInputType(InputType.TYPE_CLASS_TEXT);
         //used to enter new data into parse HighScores table
@@ -116,32 +114,53 @@ public class EndGame extends Activity {
                         playerName = input.getText().toString();
                         if(playerName.equals("")){
                             playerName = "PlayerNum_" + playerTotal;
-                            Log.d("PLAYER NUMBER", playerName);
                         }
                         ParseObject highScores = new ParseObject("HighScores");
                         highScores.put("userName", playerName);
                         highScores.put("score", scorePlayer1);
-                        highScores.saveInBackground();
-                        displayHighScore();
+                        highScores.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                displayHighScore(context);
+                            }
+                        });
                     }
                 });
         builder.show();
     }
 
 
-    public void displayHighScore(){
+    public void displayHighScore(final Context context){
         ParseQuery<ParseObject> query = ParseQuery.getQuery("HighScores");
         query.orderByAscending("score");
         query.findInBackground(new FindCallback<ParseObject>() {
             public void done(List<ParseObject> scoreList, ParseException e) {
                 if (e == null) {
                     int i = 0;
+                    boolean isTopTen = false;
                     String scores = "";
                     while(i < 10){
-                        scores = scores + scoreList.get(i).get("userName") + ":\t" + scoreList.get(i).get("score") + "\n";
+                        if(scoreList.get(i).get("userName").equals(playerName)){
+                            isTopTen = true;
+                            scores = scores + (i+1) + ". " + scoreList.get(i).get("userName") + ":\t" + scoreList.get(i).get("score") + "*" + "\n";
+                        }else{
+                            scores = scores + (i+1) + ". " + scoreList.get(i).get("userName") + ":\t" + scoreList.get(i).get("score") + "\n";
+                        }
                         i++;
                     }
-                    Log.d("NAMES", scores);
+                    if(!isTopTen){
+                        while(i < scoreList.size()){
+                            if(scoreList.get(i).get("userName").equals(playerName)){
+                                scores = scores + "\t . . . \n" + (i+1) + ". " + playerName + ":\t" + scorePlayer1;
+                                i = scoreList.size();
+                            }
+                            i++;
+                        }
+                    }
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                            .setTitle("Current High Scores")
+                            .setMessage(scores);
+                    builder.show();
                 } else {
                     Log.d("QUERY", "Error in query for HighScores: " + e.getMessage());
                 }
